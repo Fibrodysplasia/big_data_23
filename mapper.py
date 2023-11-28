68c03f35deb603abe051e20460eaae0a705071d6
Fibrodysplasia <wdmcdonough@gmail.com>
2023-11-27 22:01:53 -0600

    work on mapper
    clean up the words, seems to emit what we are looking for

--- mapper.py
+++ mapper.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env/python
 
 import sys
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+nltk.download('punkt')
+
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+
+def process_text(text):
+    # Clean up the data, see readme
+    text = re.sub(r'[^\w\s]', '', text.lower())
+    text = re.sub(r'\b[xX]+\b', '', text)
+    text = re.sub(r'\b(?:xX{2}\/xX{2}\/xX{4})\b', '', text)
+    words = word_tokenize(text)
+    words = [word for word in words if word.lower() not in stop_words]
+
+    return ' '.join(words)
 
 for line in sys.stdin:
     try:
         product, complaints = line.strip().split('\t')
-        print(f"{product}, {complaints}")
+        processed_complaints = process_text(complaints)
+        print(f"{product}, {processed_complaints}")
     except ValueError:
         pass
